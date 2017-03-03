e0061e4612df573fdae87a4cc6d601dd256d01b8
Philipp Holzschneider <philipp@covicon.de>
2017-03-03 16:44:02 +0100

    canvas ftw

---
+++ test/de/dualuse/swt/CanvasTest.java
@@ -0,0 +1,49 @@
+package de.dualuse.swt;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class CanvasTest {
+	public static void main(String[] args) {
+
+		Display dsp = new Display();
+		Shell sh = new Shell(dsp);
+		sh.setLayout(new FillLayout());
+
+		
+		Canvas c = new Canvas(sh, 0);
+		c.addPaintListener((e) -> {
+			Point dim = c.getSize();
+			e.gc.drawLine(0, 0, dim.x, dim.y);
+
+			e.gc.setAlpha(100);
+			Color col = new Color(e.gc.getDevice(), new RGB(0, 0, 0));
+			e.gc.setLineWidth(30);
+			e.gc.setLineDash(new int[] {100});
+			e.gc.setForeground(col);
+			e.gc.drawOval(100, 100, dim.x-200, dim.y-200);
+			
+			
+			
+			
+		});
+		
+		
+		sh.setBounds(100, 100, 800, 800);
+		sh.setVisible(true);
+
+		///
+		
+		while(!sh.isDisposed())
+			if (dsp.readAndDispatch())
+				if (!dsp.isDisposed())
+					dsp.sleep();
+
+		
+	}
+}
