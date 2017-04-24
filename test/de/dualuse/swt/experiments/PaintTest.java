5fa13457ce4c6981c03edd20ad9c0b00ecd38c0f
sihlefeld <sihlefeld@bentley.autonomos.xyz>
2017-04-24 13:39:36 +0200

    LineAttributes workaround
    LineAttributes workaround to 45 degree rotation bug when paths are
    drawn.

--- test/de/dualuse/swt/experiments/PaintTest.java
+++ test/de/dualuse/swt/experiments/PaintTest.java
@@ -4,9 +4,12 @@ import static org.eclipse.swt.SWT.NONE;
 
 import java.awt.geom.RoundRectangle2D;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.LineAttributes;
 import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
@@ -29,25 +32,37 @@ public class PaintTest {
 		c.addPaintListener(new PaintListener() {
 			Transform t = new Transform(app); 
 			
+			LineAttributes attr = new LineAttributes(1f, SWT.CAP_FLAT, SWT.JOIN_MITER);
+			
+			float angle = 0;
+			
 			public void paintControl(PaintEvent e) {
-				
+
+				GC gc = e.gc;
 				
 				t.identity();
 				t.translate(100,100);
 				t.scale(.5f, .5f);
+				
 				t.translate(+50, +50);
-				t.rotate(44.97f);
+				
+				t.rotate(angle);
+				
 				t.translate(-50, -50);
 				
 				e.gc.setTransform(t);
+
+				e.gc.drawImage(im, 0, 0);
+				
+//				e.gc.getGCData().state |= 1 << 14;
+				e.gc.setLineAttributes(attr);
 				
 				PathShape p = new PathShape(app, new RoundRectangle2D.Double(0,0,100,100,30,30));
 				e.gc.drawPath( p );
 				p.dispose();
 				
-				e.gc.drawImage(im, 0, 0);
-				
 //				e.gc.drawRectangle(0, 0, 100, 100);
+				angle += 0.128f;
 				c.redraw();
 			}
 		});
